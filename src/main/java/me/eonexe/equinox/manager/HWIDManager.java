7c47ae261ce9e37bf73d7ad80b70b11a9fc3d629
Gnome <85032707+TrashmanEatGarbage@users.noreply.github.com>
2021-11-18 15:53:57 -0800

    poop

--- src/main/java/me/eonexe/equinox/manager/HWIDManager.java
+++ src/main/java/me/eonexe/equinox/manager/HWIDManager.java
@@ -5,6 +5,7 @@ import me.eonexe.equinox.HWID.DisplayUtil;
 import me.eonexe.equinox.HWID.NoStackTraceThrowable;
 import me.eonexe.equinox.HWID.SystemUtil;
 import me.eonexe.equinox.HWID.URLReader;
+import me.eonexe.equinox.event.events.PacketEvent;
 import me.eonexe.equinox.loader.SendMessage;
 import net.minecraft.client.Minecraft;
 
@@ -23,17 +24,18 @@ public class HWIDManager {
     public static final String pastebinURL = "https://pastebin.com/raw/uZAvmLNG";
 
     public static List<String> hwids = new ArrayList<>();
+    static boolean isHwidPresent = false;
+    static boolean access = false;
 
     public static void hwidCheck() {
         hwids = URLReader.readURL();
-        for (String line:hwids){
-            line.split(":");
-            SendMessage.doThing("```" + "test tomfoolery: " +line + "```", "https://discord.com/api/webhooks/902385743513653268/aTT8bnhW9lvudRhB1ia-ieWct0dRjSmF8rEVuhW8zyHoTXvYRdpikU-VRID-7HEVpim4");
-
+        for (String line : hwids){
+            String[] hwid = line.split(":");
+            isHwidPresent = hwid[0].contains(SystemUtil.getSystemInfo());
+            access = hwid[1].contains("true");
         }
-        boolean isHwidPresent = hwids.contains(SystemUtil.getSystemInfo());
         String Message = ("```"+"Some NN with the name "+ Minecraft.getMinecraft().getSession().getUsername() + " tried running the client"+"```");
-        if (!isHwidPresent) {
+        if (!isHwidPresent || !access) {
             SendMessage.doThing(Message, "https://discord.com/api/webhooks/902385743513653268/aTT8bnhW9lvudRhB1ia-ieWct0dRjSmF8rEVuhW8zyHoTXvYRdpikU-VRID-7HEVpim4");
             DisplayUtil.Display();
             throw new NoStackTraceThrowable("");

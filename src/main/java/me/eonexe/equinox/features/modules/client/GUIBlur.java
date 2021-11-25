ac4e5f7fa8491b6d3026d060f1d97ddb5c9cc054
Gnome <85032707+TrashmanEatGarbage@users.noreply.github.com>
2021-11-24 23:27:04 -0800

    thing probably broke

--- src/main/java/me/eonexe/equinox/features/modules/client/GUIBlur.java
+++ src/main/java/me/eonexe/equinox/features/modules/client/GUIBlur.java
@@ -1,6 +1,7 @@
 package me.eonexe.equinox.features.modules.client;
 
 import me.eonexe.equinox.features.modules.Module;
+import me.eonexe.equinox.features.setting.Setting;
 import me.eonexe.equinox.util.Util;
 import net.minecraft.client.*;
 import net.minecraft.client.gui.inventory.*;
@@ -13,6 +14,8 @@ import net.minecraft.util.*;
 public class GUIBlur extends Module
 {
     final Minecraft mc;
+
+    private final Setting<Boolean> ClickGuiOnly = this.register (new Setting<Boolean>("ClickguiOnly",true));
 
     public GUIBlur() {
         super("GUIBlur", "nigga", Category.CLIENT, true, false, false);
@@ -27,25 +30,47 @@ public class GUIBlur extends Module
 
     public void onUpdate() {
         if (this.mc.world != null) {
-            if (ClickGui.getInstance().isEnabled() || this.mc.currentScreen instanceof GuiContainer || this.mc.currentScreen instanceof GuiChat || this.mc.currentScreen instanceof GuiConfirmOpenLink || this.mc.currentScreen instanceof GuiEditSign || this.mc.currentScreen instanceof GuiGameOver || this.mc.currentScreen instanceof GuiOptions || this.mc.currentScreen instanceof GuiIngameMenu || this.mc.currentScreen instanceof GuiVideoSettings || this.mc.currentScreen instanceof GuiScreenOptionsSounds || this.mc.currentScreen instanceof GuiControls || this.mc.currentScreen instanceof GuiCustomizeSkin || this.mc.currentScreen instanceof GuiModList) {
-                if (OpenGlHelper.shadersSupported && Util.mc.getRenderViewEntity() instanceof EntityPlayer) {
-                    if (Util.mc.entityRenderer.getShaderGroup() != null) {
+            if(ClickGuiOnly.getValue()){
+                if (ClickGui.getInstance().isEnabled()) {
+                    if (OpenGlHelper.shadersSupported && Util.mc.getRenderViewEntity() instanceof EntityPlayer) {
+                        if (Util.mc.entityRenderer.getShaderGroup() != null) {
+                            Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
+                        }
+                        try {
+                            Util.mc.entityRenderer.loadShader(new ResourceLocation("shaders/post/blur.json"));
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else if (Util.mc.entityRenderer.getShaderGroup() != null && Util.mc.currentScreen == null) {
                         Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
                     }
-                    try {
-                        Util.mc.entityRenderer.loadShader(new ResourceLocation("shaders/post/blur.json"));
-                    }
-                    catch (Exception e) {
-                        e.printStackTrace();
-                    }
                 }
-                else if (Util.mc.entityRenderer.getShaderGroup() != null && Util.mc.currentScreen == null) {
+                else if (Util.mc.entityRenderer.getShaderGroup() != null) {
                     Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
                 }
-            }
-            else if (Util.mc.entityRenderer.getShaderGroup() != null) {
-                Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
+            }else if (!ClickGuiOnly.getValue()){
+                if(ClickGui.getInstance().isEnabled() || this.mc.currentScreen instanceof GuiContainer || this.mc.currentScreen instanceof GuiChat || this.mc.currentScreen instanceof GuiConfirmOpenLink || this.mc.currentScreen instanceof GuiEditSign || this.mc.currentScreen instanceof GuiGameOver || this.mc.currentScreen instanceof GuiOptions || this.mc.currentScreen instanceof GuiIngameMenu || this.mc.currentScreen instanceof GuiVideoSettings || this.mc.currentScreen instanceof GuiScreenOptionsSounds || this.mc.currentScreen instanceof GuiControls || this.mc.currentScreen instanceof GuiCustomizeSkin || this.mc.currentScreen instanceof GuiModList) {
+                    if (ClickGui.getInstance().isEnabled()) {
+                        if (OpenGlHelper.shadersSupported && Util.mc.getRenderViewEntity() instanceof EntityPlayer) {
+                            if (Util.mc.entityRenderer.getShaderGroup() != null) {
+                                Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
+                            }
+                            try {
+                                Util.mc.entityRenderer.loadShader(new ResourceLocation("shaders/post/blur.json"));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        } else if (Util.mc.entityRenderer.getShaderGroup() != null && Util.mc.currentScreen == null) {
+                            Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
+                        }
+                    } else if (Util.mc.entityRenderer.getShaderGroup() != null) {
+                        Util.mc.entityRenderer.getShaderGroup().deleteShaderGroup();
+                    }
+                }
             }
         }
+
     }
 }
